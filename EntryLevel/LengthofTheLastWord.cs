ffce266ca7d2f0072c4f0753c124845f5dbfda41
Gazwan Nagm <33843358+gazwannagm@users.noreply.github.com>
2023-10-29 23:08:40 +0300

    change the solution of  Length of the Last Word

--- EntryLevel/LengthofTheLastWord.cs
+++ EntryLevel/LengthofTheLastWord.cs
@@ -1,16 +1,10 @@
 public class Solution {
     public int LengthOfLastWord(string s) {
          s = s.TrimEnd(); 
-
         if (string.IsNullOrEmpty(s)) {
             return 0; 
         }
-
         int lastIndex = s.LastIndexOf(' ');
-        if (lastIndex == -1) {
-            return s.Length; 
-        }
-
         string lastword = s.Substring(lastIndex + 1);
         return lastword.Length;
     }
